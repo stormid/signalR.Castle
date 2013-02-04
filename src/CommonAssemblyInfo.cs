9f0f0e892f9ab88920177d62d039b5f619d0910b
Chris Canal <chris.canal@stormid.com>
2013-02-04 16:21:25 +0000

    Initial commit

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("Git commit hash: git unavailable -  - FluentMvc is a library for adding conventions to ASP.NET MVC using a fluent configuration API.")]
+[assembly: AssemblyProduct("SignalR.Castle")]
+[assembly: AssemblyCopyright("Copyright 2009-2011 Chris Canal. All rights reserved.")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.13035")]
+
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: ComVisibleAttribute(false)]
+
